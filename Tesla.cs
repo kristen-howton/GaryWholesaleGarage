f5e8a62f2cd65b0523a4c24074df448c1ac35aa0
Kristen Howton <kristen.howton@gmail.com>
2020-06-04 09:26:40 -0500

    folders set up

---
+++ Tesla.cs
@@ -0,0 +1,14 @@
+namespace Garage
+{
+    public class Tesla // Electric car
+    {
+        public double BatteryKWh { get; set; }
+        public string MainColor { get; set; }
+        public string MaximumOccupancy { get; set; }
+
+        public void ChargeBattery()
+        {
+            // method definition omitted
+        }
+    }
+}
